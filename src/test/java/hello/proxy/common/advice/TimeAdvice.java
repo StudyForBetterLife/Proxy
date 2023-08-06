1c79d664ec162c6b9c729f4c061ea127c28d62a1
hjoon <guswns3371@gmail.com>
2023-08-06 18:45:00 +0900

    JDK 동적 프록시 - 적용1,2

---
+++ src/test/java/hello/proxy/common/advice/TimeAdvice.java
@@ -0,0 +1,24 @@
+package hello.proxy.common.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+/**
+ * CGLIB는 프록시 실행 로직을 위해 MethodInterceptor라는 인터페이스를 제공한다.
+ */
+@Slf4j
+public class TimeAdvice implements MethodInterceptor {
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+        Object result = invocation.proceed();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}", resultTime);
+        return result;
+    }
+}
